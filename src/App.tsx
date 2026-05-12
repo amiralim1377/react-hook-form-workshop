0f7402a54f13787c48141aebf45571c76f9cc8b1
AMIRALI <alimoradi7718@gmail.com>
2026-05-12 12:21:17 +0330

    make form-layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div>form</div>;
+  return (
+    <div className="container min-h-screen flex items-center justify-center text-white">
+      <div className="mx-5">food delivery form</div>
+    </div>
+  );
 }
 
 export default App;
