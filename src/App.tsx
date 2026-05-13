26f04ac1595465114dfd707b68c41182ff2aa44c
AMIRALI <alimoradi7718@gmail.com>
2026-05-13 12:14:46 +0330

    add TypicalForm to compare render-count

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import FoodDeliveryForm from "./FoodDeliveryForm";
+import TypicalForm from "./TypicalForm";
 
 function App() {
   return (
-    <div className="container min-h-screen flex items-center justify-center text-white">
+    <div className="container min-h-screen flex items-center justify-center text-white flex-col gap-2">
       <FoodDeliveryForm />
+      <TypicalForm />
     </div>
   );
 }
