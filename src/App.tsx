0ef51d88fc095b04036f23478c6710dc52f4d084
AMIRALI <alimoradi7718@gmail.com>
2026-05-13 10:45:28 +0330

    FoodDeliveryForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import FoodDeliveryForm from "./FoodDeliveryForm";
 
 function App() {
   return (
     <div className="container min-h-screen flex items-center justify-center text-white">
-      <div className="mx-5">food delivery form</div>
+      <FoodDeliveryForm />
     </div>
   );
 }
