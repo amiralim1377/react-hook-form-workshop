2bbc861446716219dc858a275bd276c23e084a5c
AMIRALI <alimoradi7718@gmail.com>
2026-05-13 12:16:10 +0330

    comment-TypicalForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import FoodDeliveryForm from "./FoodDeliveryForm";
-import TypicalForm from "./TypicalForm";
 
 function App() {
   return (
     <div className="container min-h-screen flex items-center justify-center text-white flex-col gap-2">
       <FoodDeliveryForm />
-      <TypicalForm />
+      {/* <TypicalForm /> */}
     </div>
   );
 }
