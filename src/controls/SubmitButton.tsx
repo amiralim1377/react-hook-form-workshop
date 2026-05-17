269422d1f6689c4312ed78d8f6d3c8903158b297
AMIRALI <alimoradi7718@gmail.com>
2026-05-17 18:19:32 +0330

    add control-property to get isSubmitting from useFormState instead of prop-Drilling

--- src/controls/SubmitButton.tsx
+++ src/controls/SubmitButton.tsx
@@ -1,26 +1,40 @@
 import type React from "react";
+import { useRenderCount } from "../components/useRenderCount";
+import { useFormState, type Control } from "react-hook-form";
 
 type SubmitButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  isSubmitting?: boolean;
+  // eslint-disable-next-line
+  control?: Control<any, any>;
 };
 
+// eslint-disable-next-line
+const RenderCount = useRenderCount();
+
 const SubmitButton = (props: SubmitButtonProps) => {
-  const { isSubmitting = undefined, className, value, ...other } = props;
+  const { className, value, control = undefined, ...other } = props;
+  const { isSubmitting } = control
+    ? // eslint-disable-next-line
+      useFormState({ control })
+    : { isSubmitting: false };
+
   return (
-    <button
-      type="submit"
-      className={`px-2 py-1 w-18 flex items-center justify-center text-center rounded-md border border-gray-400 ${className}`}
-      disabled={isSubmitting == undefined ? false : isSubmitting}
-      {...other}
-    >
-      {isSubmitting === undefined || isSubmitting === false ? (
-        value
-      ) : (
-        <>
-          <div className="w-6 h-6 border-4  border-white border-t-transparent rounded-full animate-spin"></div>
-        </>
-      )}
-    </button>
+    <>
+      <RenderCount />
+      <button
+        type="submit"
+        className={`px-2 py-1 w-18 flex items-center justify-center text-center rounded-md border border-gray-400 ${className}`}
+        disabled={isSubmitting == undefined ? false : isSubmitting}
+        {...other}
+      >
+        {isSubmitting === undefined || isSubmitting === false ? (
+          value
+        ) : (
+          <>
+            <div className="w-6 h-6 border-4  border-white border-t-transparent rounded-full animate-spin"></div>
+          </>
+        )}
+      </button>
+    </>
   );
 };
 
