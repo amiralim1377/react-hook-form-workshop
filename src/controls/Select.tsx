873aa87bd47b94c0c07663de0b4de6916c999678
AMIRALI <alimoradi7718@gmail.com>
2026-05-19 15:51:31 +0330

    add label optional

--- src/controls/Select.tsx
+++ src/controls/Select.tsx
@@ -4,7 +4,7 @@ import type { SelectOptionType } from "../types";
 
 type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
   className?: string;
-  label: string;
+  label?: string;
   error?: FieldError | undefined;
   options: SelectOptionType[];
 };
@@ -14,12 +14,12 @@ const Select = (props: SelectProps) => {
   const { className = "", label, error, options, ...otherProps } = props;
 
   return (
-    <div className="flex  relative flex-col border px-2 py-1 pb-3 border-gray-400 rounded-md">
-      <label htmlFor={id}>{label}</label>
+    <div className="flex  relative flex-col border  px-2 py-1 pb-3 border-gray-400 rounded-md">
+      {label && <label htmlFor={id}>{label}</label>}
       <select
         className={`w-full outline-0 px-3 py-2 rounded-md border border-gray-400
          bg-zinc-800 text-white
-         cursor-pointer ${className}`}
+         cursor-pointer  ${className}`}
         id={id}
         {...otherProps}
       >
